15431ff3ebd87f4c102f2240788eb8512b96a6d1
zhaiduting <zhaiduting@163.com>
2023-05-05 05:29:25 +0000

    Show winner

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   let [XO, setXO] = useState("X");
   let status;
-  status = "Next player: " + XO;
+  let winner = caculateWinner();
+  status = winner ? "Winner: " + winner : "Next player: " + XO;
   function handleClick(i) {
-    if (squares[i]) return;
+    if (squares[i] || caculateWinner()) return;
     const nextSquares = squares.slice();
     nextSquares[i] = XO;
     setSquares(nextSquares);
@@ -39,4 +40,21 @@ export default function Board() {
       </div>
     </>
   );
+  function caculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      let [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+        return squares[a];
+    }
+  }
 }
